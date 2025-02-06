62b1ac118a358087823b4f0bae59d3307d3d7eed
itaouo <158411654+itaouo@users.noreply.github.com>
2025-02-06 23:09:54 +0800

    [Refactoring] Extract the weather parser into a .js that can easily be reused.

--- command/weather.js
+++ command/weather.js
@@ -1,10 +1,5 @@
-const axios = require('axios')
+const weatherParser = require('../tools/weather-parser.js')
 
-require('dotenv').config()
-const WEATHER_URL = process.env.WEATHER_URL
-const WEATHER_SPOTS_SAVE = process.env.WEATHER_SPOTS_SAVE
-
-const { readFile, stringToList } = require('../tools/file-handler.js')
 const slashCommandName = 'weather'
 
 const slashCommand = () => {
@@ -15,33 +10,19 @@ const slashCommand = () => {
 }
 
 const execute = async (options) => {
-    let citys = await stringToList(await readFile(WEATHER_SPOTS_SAVE))
-    if (citys.length === 0) { return "沒有任何地點嗚嗚\n快下 /subscribe_weather 新增一個地點鴨" }
-
-    const response = await axios.get(WEATHER_URL)
-    const locations = response.data.records.location
-    const hours = new Date().getHours()
-    let content = ""
-
-    locations.forEach((location) => {
-        if(citys.includes(location.locationName)){
-            const elements = location.weatherElement
-            let name = location.locationName
-            let date = elements[0].time[1].startTime.split(' ')[0]
-            let minT = "NaN"
-            let maxT = "NaN"
-            let pop = "NaN"
     
-            elements.forEach((element) => {
-            if (element.elementName === "MinT") { minT = element.time[1].parameter.parameterName }
-            if (element.elementName === "MaxT") { maxT = element.time[1].parameter.parameterName }
-            if (element.elementName == "PoP"){ pop = element.time[1].parameter.parameterName }
-            })
-            
-            if (hours < 6) { content += `今天 (${date}) ${name} ${minT}~${maxT} 度，有 ${pop}% 的機率會下雨鴨\n` }
-            else{ content += `明天 (${date}) ${name} ${minT}~${maxT} 度，有 ${pop}% 的機率會下雨鴨\n` }
-        }
-    })
+    let content = ""
+    
+    const hours = new Date().getHours()
+    const datas = await weatherParser()
+    
+    if (datas.length === 0) { return "沒有任何地點嗚嗚\n快下 /subscribe_weather 新增一個地點" }
+    datas.forEach(data => {
+        if (hours < 6) { content += `今天 (${data.date}) ${data.name} ${data.minT}~${data.maxT} 度，有 ${data.pop}% 的機率會下雨\n` }
+        else{ content += `明天 (${data.date}) ${data.name} ${data.minT}~${data.maxT} 度，有 ${data.pop}% 的機率會下雨\n` }
+        console.log(data.name)
+    });
+    
     return content
 }
 
---
+++ tools/weather-parser.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+
+require('dotenv').config()
+const WEATHER_URL = process.env.WEATHER_URL
+const WEATHER_SPOTS_SAVE = process.env.WEATHER_SPOTS_SAVE
+
+const { readFile, stringToList } = require('../tools/file-handler.js')
+
+const weatherParser = async () => {
+    let citys = await stringToList(await readFile(WEATHER_SPOTS_SAVE))
+    let datas = []
+    
+    const response = await axios.get(WEATHER_URL)
+    const locations = response.data.records.location
+    
+    locations.forEach((location) => {
+        if(citys.includes(location.locationName)){
+            const elements = location.weatherElement
+
+            let date = elements[0].time[1].startTime.split(' ')[0]
+            let name = location.locationName
+            let minT = "NaN"
+            let maxT = "NaN"
+            let pop = "NaN"
+
+            elements.forEach((element) => {
+            if (element.elementName === "MinT") { minT = element.time[1].parameter.parameterName }
+            if (element.elementName === "MaxT") { maxT = element.time[1].parameter.parameterName }
+            if (element.elementName == "PoP"){ pop = element.time[1].parameter.parameterName }
+            })
+            datas.push({ date, name, minT, maxT, pop })
+        }
+    })
+    return datas
+}
+
+module.exports = weatherParser
